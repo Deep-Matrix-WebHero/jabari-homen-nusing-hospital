dd1170d2e89b710a151eb46d3780a2c733a8a617
Md Ataur Rahman <81749481+Deep-Matrix-WebHero@users.noreply.github.com>
2021-09-27 21:45:39 +0600

    headers

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className = "header">
+           <h1>Jabari Home Nursing Services</h1>
+           <h4>We assembled and icredible team of experienced Qualified </h4>
+           <h4>Caregivers who are truly passionate about and committed to providing the highest quality care.</h4> 
+           <h1>Jabari Home Nursing Services Total  Budgets: TK-5 Million </h1>
+        </div>
+    );
+};
+
+export default Header;
